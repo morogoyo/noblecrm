9415c1f1a601bf76efb4e9c9f470f88f52141f66
morogoyo <morogoyo@gmail.com>
2020-08-06 15:55:52 -0400

    fixed database connectivity issue problem was conflicting versions of JPA

--- src/main/java/com/nobledigitalservice/noblecrm/jwt/service/JwtInDBUserDetailsService.java
+++ src/main/java/com/nobledigitalservice/noblecrm/jwt/service/JwtInDBUserDetailsService.java
@@ -1,6 +1,9 @@
 package com.nobledigitalservice.noblecrm.jwt.service;
 
 import com.nobledigitalservice.noblecrm.jwt.model.JwtUserDetails;
+import com.nobledigitalservice.noblecrm.jwt.model.UserDTO;
+import com.nobledigitalservice.noblecrm.jwt.repository.UserDTORepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
@@ -9,28 +12,29 @@ import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 
 @Service
 @Qualifier("inDatabase")
 public class JwtInDBUserDetailsService implements UserDetailsService {
 
-//    @Autowired
-//    private UserDTORepository userDTO;
+    @Autowired
+    private UserDTORepository userDTO;
 
     static List<JwtUserDetails> inMemoryUserList = new ArrayList<>();
 
 
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
 
-//        Optional<UserDTO> user = Optional.of(userDTO.findByUserName(username));
+        Optional<UserDTO> user = Optional.of(userDTO.findByUserName(username));
 
 
-//        if (!user.isPresent()){
-//            throw new UsernameNotFoundException("User not found for username "+ username);
-//        }
-        JwtUserDetails jwtUser = null;
-////        JwtUserDetails jwtUser = new JwtUserDetails(user.get().getId(),user.get().getUserName(),user.get().getPassword(),user.get().getRole());
+        if (!user.isPresent()){
+            throw new UsernameNotFoundException("User not found for username "+ username);
+        }
+
+        JwtUserDetails jwtUser = new JwtUserDetails(user.get().getId(),user.get().getUserName(),user.get().getPassword(),user.get().getRole());
         return jwtUser;
 
     }
